2be1f9332e7b64890c27979488bf32f012a8ec5e
cowson <nielsgraham@gmail.com>
2016-10-28 10:09:19 -0400

    added code

---
+++ stat.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+
+int main(){
+  struct stat fileStat;
+  stat("test.txt", &fileStat);
+
+  printf("Information for %s\n", "test.txt");
+  printf("---------------------------\n");
+  printf("File Size: \t\t%d bytes\n",fileStat.st_size);
+  printf("Time of last access: \t\t %d\n", fileStat.st_atime);
+
+  printf("File Permissions: \t");
+  printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+  printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+  printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+  printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+  printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+  printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+  printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+  printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+  printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+  printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+  printf("\n\n");
+  return 0;
+}
